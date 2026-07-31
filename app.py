8c06579d7df648f0df1c59271c0e526b6154d6df
IDLabs <idris@polibatam.ac.id>
2026-08-01 06:17:38 +0700

    revert: clean revert of PrefixMiddleware and X-Forwarded-Prefix headers to keep app.py 100% clean

--- app.py
+++ app.py
@@ -6,25 +6,11 @@ from werkzeug.security import generate_password_hash
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 
-class PrefixMiddleware:
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        prefix = environ.get('HTTP_X_FORWARDED_PREFIX', '') or environ.get('HTTP_X_SCRIPT_NAME', '')
-        if prefix:
-            environ['SCRIPT_NAME'] = prefix
-            path_info = environ.get('PATH_INFO', '')
-            if path_info.startswith(prefix):
-                environ['PATH_INFO'] = path_info[len(prefix):]
-        return self.app(environ, start_response)
-
 ph = PasswordHasher()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.wsgi_app = PrefixMiddleware(app.wsgi_app)
 
     db.init_app(app)
     migrate.init_app(app, db)
